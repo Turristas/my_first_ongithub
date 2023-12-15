7d3fe7227ebbf78da69439b6ac66308c679cf4f1
turristas <tur.ristas@yahoo.com>
2023-12-15 16:23:03 +0300

    4.0

--- some_file.py
+++ some_file.py
@@ -3,3 +3,8 @@ print("This is a file from GitHub")
 print('these are new local chenge')
 
 print('/\_/\\\n>^,^<\n / \\\n(|_|)_/')
+
+def get_string(string: str, times: int, sep: str=',') -> str:
+    return sep.join([string] * times)
+    
+print(get_string('t', 12))
